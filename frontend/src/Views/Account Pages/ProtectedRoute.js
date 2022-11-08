d051bb3ea70674b4f2cf55bc5e7c3dc83fd6119f
Pritisha Shrestha <59680460+Pritishashrestha@users.noreply.github.com>
2022-11-08 14:03:48 +0545

    reset

--- frontend/src/Views/Account Pages/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-export const ProtectedRoute = () => {
-  const loggedIn = localStorage.getItem("authToken") ? true : false;
-  // console.log("loggedIn", loggedIn);
-  return loggedIn ? <Outlet /> : <Navigate to="/signin" />;
-};
